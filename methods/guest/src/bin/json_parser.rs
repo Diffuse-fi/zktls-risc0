7a8e3d049b97a1d0663fd0d179f50b70882e6317
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2024-12-10 14:18:53 +0100

    cargo fmt all

--- methods/guest/src/bin/json_parser.rs
+++ methods/guest/src/bin/json_parser.rs
@@ -14,34 +14,52 @@
 
 // use alloy_primitives::U256;
 use alloy_sol_types::SolValue;
-use risc0_zkvm::guest::env;
 use json::{parse, JsonValue};
+use risc0_zkvm::guest::env;
 
-fn extract_pair_data<'a>(json_data: &'a JsonValue, pair_name: &'a String) -> (&'a String, u64, u64) {
-
+fn extract_pair_data<'a>(
+    json_data: &'a JsonValue,
+    pair_name: &'a String,
+) -> (&'a String, u64, u64) {
     let price_str = json_data[pair_name]["price"].to_string();
 
     let integer_and_fractional: Vec<&str> = price_str.split('.').collect();
 
     if integer_and_fractional.len() != 2 {
-        eprintln!("Error: 'price' value for {} is {}, unable to parse as float", pair_name, price_str);
+        eprintln!(
+            "Error: 'price' value for {} is {}, unable to parse as float",
+            pair_name, price_str
+        );
         panic!("price is not float number!");
     }
 
-    let integer: u64 = integer_and_fractional[0].parse().expect("Failed to parse integer part");
-    let fractional: u64 = integer_and_fractional[1].parse().expect("Failed to parse integer part");
+    let integer: u64 = integer_and_fractional[0]
+        .parse()
+        .expect("Failed to parse integer part");
+    let fractional: u64 = integer_and_fractional[1]
+        .parse()
+        .expect("Failed to parse integer part");
 
     let mut price: u64 = integer * 100000;
-    let decimal_points: u32 = integer_and_fractional[1].chars().count().try_into().unwrap();
-    assert!(decimal_points <= 5, "price decimal points <= 5 are hardcoded"); // TODO 5 hardcoded
+    let decimal_points: u32 = integer_and_fractional[1]
+        .chars()
+        .count()
+        .try_into()
+        .unwrap();
+    assert!(
+        decimal_points <= 5,
+        "price decimal points <= 5 are hardcoded"
+    ); // TODO 5 hardcoded
 
     price += fractional * 10u64.pow(5 - decimal_points);
-
 
     let timestamp = match json_data[pair_name]["closeTime"].as_u64() {
         Some(value) => value,
         None => {
-            eprintln!("Error: 'closeTime' value for {} pair is not a valid u64 or is missing", pair_name);
+            eprintln!(
+                "Error: 'closeTime' value for {} pair is not a valid u64 or is missing",
+                pair_name
+            );
             panic!("Failed to parse timestamp");
         }
     };
@@ -50,7 +68,6 @@ fn extract_pair_data<'a>(json_data: &'a JsonValue, pair_name: &'a String) -> (&'
 }
 
 fn main() {
-
     // TODO declared identical to methods/src/guest_input_struct.rs because was failing to import for too long
     #[derive(serde::Deserialize)]
     pub struct GuestInputType {
@@ -68,7 +85,6 @@ fn main() {
     for i in 0..4 {
         results[i] = extract_pair_data(&json_data, &input.currency_pairs[i]);
     }
-
 
     // Commit the journal that will be received by the application contract.
     // Journal is encoded using Solidity ABI for easy decoding in the app contract.
