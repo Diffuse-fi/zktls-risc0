51e0ff1b253ec51e706eaa188945acdbd210c086
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2024-12-10 14:59:27 +0100

    check localhost:8545 availability in cli test

--- cli/test.py
+++ cli/test.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import sys
+import socket
 
 from utils.network import *
 from deploy_feeder import deploy_data_feeder, request_storage_addresses
@@ -10,6 +11,15 @@ from feed_feeder import feed_data_legacy
 from request_storage import do_request
 from request_storage import method_enum
 from parse_and_prove import prepare_json
+
+
+def is_port_open(host, port):
+    try:
+        with socket.create_connection((host, port), timeout=60):
+            return True
+    except (socket.timeout, ConnectionRefusedError):
+        return False
+
 
 anvil_testnet_private_key = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 os.environ["ETH_WALLET_PRIVATE_KEY"] = anvil_testnet_private_key
@@ -21,6 +31,14 @@ if process.poll() is None:
 else:
     print("failed to start anvil")
     sys.exit(process.poll())
+
+
+if is_port_open("localhost", 8545):
+    print("Connection to localhost:8545 succeeded")
+else:
+    print("Connection to localhost:8545 failed")
+    sys.exit(1)
+
 
 print("step 2: deploying datafeed feeder...")
 local_network = network_enum.LOCAL
